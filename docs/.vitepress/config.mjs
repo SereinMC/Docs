193f8c81723709ec72bc72c3e44877fe28f5bfec
BuShe Pie <bushe.cn@icloud.com>
2023-08-23 23:48:43 +0800

    vitepress: update ase

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress';
 export default defineConfig({
 	title: 'Serein Documents',
 	description: 'Serein Documents',
+	base: '/Docs/',
 	themeConfig: {
 		// https://vitepress.dev/reference/default-theme-config
 		socialLinks: [{ icon: 'github', link: 'https://github.com/SereinMC/serein' }]
